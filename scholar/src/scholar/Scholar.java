5d6bc943be41ddd2a5ef02ade57b2eb0156b5d9a
angello <angello1998616@gmail.com>
2019-09-25 08:09:51 -0500

    actualizacion 4

--- scholar/src/scholar/Scholar.java
+++ scholar/src/scholar/Scholar.java
@@ -15,23 +15,26 @@ public class Scholar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-        Empleado empleado1= new Empleado(); 
-         
-        empleado1.nombre= "angello";
-        empleado1.apellido= "maury";
-        //System.out.println("el empleado se llama" + empleado1.nombre + empleado1.apellido);
-        empleado1.printname();
-        Estudiante estudiante1= new Estudiante();
-
+  
+        ventana2 v2 = new ventana2();
+        v2.setVisible(true);        
         
-       estudiante1.nombre= "angello";
-       estudiante1.fecha_nacimiento= "1998-12-06";
-        System.out.println("la fecha de nacimiento es" + estudiante1.fecha_nacimiento);
-      empleado1.printfecha();
-      
-       //System.out.println("la edad es: " + this.calcEdad() );
-       estudiante1.printedad();
+//        Empleado empleado1= new Empleado(); 
+//         
+//        empleado1.nombre= "angello";
+//        empleado1.apellido= "maury";
+//        //System.out.println("el empleado se llama" + empleado1.nombre + empleado1.apellido);
+//        empleado1.printname();
+//        Estudiante estudiante1= new Estudiante();
+//
+//        
+//       estudiante1.nombre= "angello";
+//       estudiante1.fecha_nacimiento= "1998-12-06";
+//        System.out.println("la fecha de nacimiento es" + estudiante1.fecha_nacimiento);
+//      empleado1.printfecha();
+//      
+//       //System.out.println("la edad es: " + this.calcEdad() );
+//       estudiante1.printedad();
           
         
        
